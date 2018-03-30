3d51e2a9b7b0390064e260699ed3450c51a2bbcb
soxueren <250029975@qq.com>
2018-03-30 11:29:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ USER_NAME=`ls -lah / | grep mapproxy | awk '{print $3}'`
 cd /mapproxy
 
 su $USER_NAME -c "uwsgi --ini uwsgi.conf --daemonize /var/log/uwsgi.log --post-buffering 32768 --buffer-size 32768"
-su $USER_NAME -c "nginx -g daemon off"
+#su $USER_NAME -c "nginx -g daemon off"
 
 
